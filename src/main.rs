f60a8bf75c2fd895bdf462024c7f0d5691d599fc
SwimmingPigLuvr <dnevell95@gmail.com>
2022-10-19 08:38:41 -0700

    ✅📦 checking off some more boxes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
 use owo_colors::OwoColorize;
 use std::fs::{File, self};
 use std::io::prelude::*;
+use std::io::Read;
 
 // BUDGET APP
 
 // TO LEARN: everything
 
 // ✅ write to a file 
-// write to file without deleting contents
-// read from file
+// ✅ write to file without deleting contents
+// ✅ read from file
 // parse input as expenses & earnings
-// 
+// use a calendar crate so my inputs will be timestamped
+
+// 10-19-22
+// user experience
+// type budget in terminal to open
+
+// BUDGET APP
+
+// hello 'username here', what would you like to do?
+
+// select option:
+// 'view expenses'
+// 'view earnings'
+// 'input new item'
+// 'delete item'
+
+
 
 fn main() -> std::io::Result<()> {
     
@@ -42,11 +59,28 @@ fn main() -> std::io::Result<()> {
             .dimmed()
     );
     
-    let mut budget = File::create("budget.txt")?;
+    // input
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).expect("cannot read input are you sure you typed that in correctly?");
     let item = input.trim();
+
+    // write input to "budget.txt"
     fs::write("budget.txt", item)?;
+
+    // read "budget.txt" to budget_string
+    let mut budget_file = fs::OpenOptions::new()
+        // .read(true)
+        .append(true)
+        .open("budget.txt")?;
+
+    println!("{}", ("input received, enter another item").cyan());
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("cannot read input are you sure you typed that in correctly?");
+    let item = input.trim();
+    let to_bytes = input.as_bytes();
+    
+    // write input to "budget.txt"
+    budget_file.write_all(to_bytes)?;
 
     Ok(())
 }
