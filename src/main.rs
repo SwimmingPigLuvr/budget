db1907e4309a02d6ad23a635e85ed5a7e3791fcd
SwimmingPigLuvr <dnevell95@gmail.com>
2022-10-20 11:16:44 -0700

    🏂🏻progress

--- src/main.rs
+++ src/main.rs
@@ -2,85 +2,53 @@ use owo_colors::OwoColorize;
 use std::fs::{File, self};
 use std::io::prelude::*;
 use std::io::Read;
-
-// BUDGET APP
-
-// TO LEARN: everything
-
-// ✅ write to a file 
-// ✅ write to file without deleting contents
-// ✅ read from file
-// parse input as expenses & earnings
-// use a calendar crate so my inputs will be timestamped
-
-// 10-19-22
-// user experience
-// type budget in terminal to open
-
-// BUDGET APP
-
-// hello 'username here', what would you like to do?
-
-// select option:
-// 'view expenses'
-// 'view earnings'
-// 'input new item'
-// 'delete item'
+use serde_derive::{Deserialize, Serialize};
+use serde_json::{Result, Value, json};
 
 
 
-fn main() -> std::io::Result<()> {
-    
 
-    
 
-    println!("\n\n{}", ("BUDGET APP").green());
-    println!(
-        "{}",
-        ("-----------------------------------------------")
-            .green()
-            .dimmed()
-    );
-    println!(
-        "{} {} {} {} {} {} {} {}",
-        ("Input").green().dimmed(),
-        ("+").green(),
-        ("or").green().dimmed(),
-        ("-").red(),
-        ("the").green().dimmed(),
-        ("dollar amount").cyan(),
-        ("followed by").green().dimmed(),
-        ("item").cyan()
-    );
-    println!(
-        "{}\n",
-        ("-----------------------------------------------")
-            .green()
-            .dimmed()
-    );
-    
-    // input
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Item {
+    name: String,
+    value: i32,
+}
+
+
+// build item
+fn set_item(name: String, value: i32) -> Item {
+    let new_item: Item = Item {
+        name,
+        value
+    };
+    new_item
+}
+
+
+fn main() -> Result<()> {
+
+    println!("{}", ("enter the name and the dollar amount").green().dimmed());
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("cannot read input are you sure you typed that in correctly?");
-    let item = input.trim();
+    std::io::stdin().read_line(&mut input).expect("cant read that shit");
+    let trim = input.trim();
+    let input_vec: Vec<&str> = trim.split_terminator(&['+', '-'][..]).collect();
+    let name = input_vec[0];
+    let value = input_vec[1].parse().unwrap();
 
-    // write input to "budget.txt"
-    fs::write("budget.txt", item)?;
+    let item1 = set_item(name.to_owned(), value);
 
-    // read "budget.txt" to budget_string
-    let mut budget_file = fs::OpenOptions::new()
-        // .read(true)
-        .append(true)
-        .open("budget.txt")?;
+    println!("{} {:?}", ("first item:"), item1);
 
-    println!("{}", ("input received, enter another item").cyan());
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("cannot read input are you sure you typed that in correctly?");
-    let item = input.trim();
-    let to_bytes = input.as_bytes();
+    // we have successfully taken input and created a new item struct with it
+    // now we can parse this struct into json and print the json to "budget.json"
+    let item1_json = serde_json::to_string(&item1)?;
+
     
-    // write input to "budget.txt"
-    budget_file.write_all(to_bytes)?;
+    println!("{}", item1_json.cyan().blink());
+
 
     Ok(())
+
 }
